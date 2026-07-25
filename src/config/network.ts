8839948fef99ef4684ef3435a3f8e080d70195fb
Aron Park <arondev1007@gmail.com>
2026-07-25 13:07:19 +0900

    fix: gnoscan url

--- src/config/network.ts
+++ src/config/network.ts
@@ -1,13 +1,7 @@
 export const GNOSCAN_URL = 'https://gnoscan.io'
 
-export const GNO_RPC_URL = import.meta.env.VITE_GNO_RPC_URL || 'https://topaz.rpc.onbloc.xyz'
-
 export const makeGnoscanTransactionUrl = (txHash: string): string => {
-  const params = new URLSearchParams({
-    type: 'custom',
-    rpcUrl: GNO_RPC_URL,
-    txhash: txHash,
-  })
+  const params = new URLSearchParams({ txhash: txHash })
 
   return `${GNOSCAN_URL}/transactions/details?${params.toString()}`
 }
